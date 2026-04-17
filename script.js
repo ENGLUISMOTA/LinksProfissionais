eb27c8d595bce092b1e8bd58e6c902c2cae90220
ENGLUISMOTA <EngLuisMota@gmail.com>
2026-04-17 16:48:01 -0300

    Ajuste final no nome da imagem de perfil

---
+++ script.js
@@ -0,0 +1,7 @@
+// Exemplo simples: Adicionando um log de boas-vindas
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Linktree Clone carregado com sucesso!");
+    
+    // Você pode usar este espaço para buscar dados de uma API no futuro
+    // Ou simplesmente para manipular efeitos de clique.
+});
